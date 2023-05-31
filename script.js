df71b88e73c21994ee8642ebbafbb23a4e90ad8c
Yassen <nwbyaseen@gmail.com>
2023-06-01 01:22:23 +0500

    Designing done

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 console.log('Welcome to tic tac to');
 let music = new Audio('music.mp3');
 let audioturn = new Audio ('ting.mp3');
-let gameover = new Audio('gamover.mp3');
-let turn ="X";
+let gameover = new Audio('gameover.mp3');
+let Turn ="X";
 let thisgame = false;
 
 
 // Function to change the turn
 const changeTurn = ()=>{
-    return turn === "X"?"0":"X"
+    return Turn === "X"?"0":"X"
 }
   
 
@@ -16,20 +16,23 @@ const changeTurn = ()=>{
 const checkwin =()=>{
     let boxtext = document.getElementsByClassName('boxtext');
     let win =[
-        [1,2,3],
-        [4,5,6],
-        [7,8,9],
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
         [1,4,7],
         [2,5,8],
-        [3,6,9],
-        [1,5,9],
-        [3,5,7],
+        [0,4,8],
+        [2,4,6]
+     
     ]
     win.forEach(e =>{
-        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText===boxtext[e[1]].innerText) && (boxtext[e[0]].innerText1!=='')){
+        if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !=='')){
             document.querySelector('.info').innerText = boxtext[e[0]].innerText+'Won'
             thisgame=true
-        }
+            // document.querySelector('.imgif').getElementsByTagName('img').style.width='200px';
+            document.querySelector('.imgif').getElementsByTagName('img')[0].style.width='200px'
+        } 
     })
 
 }
@@ -38,14 +41,14 @@ const checkwin =()=>{
 let box = document.getElementsByClassName('box');
 Array.from(box).forEach(element =>{
     let boxtext = element.querySelector('.boxtext');
-    element.addEventListener('click',() =>{
-        if(boxtext.innerText ===''){
-            boxtext.innerText=turn;
-            turn=changeTurn();
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText === ''){
+            boxtext.innerText = Turn;
+            Turn=changeTurn();
             audioturn.play();
             checkwin();
             if(!thisgame){
-                document.getElementsByClassName('info')[0].innerText= 'Turn for'+turn;
+                document.getElementsByClassName('info')[0].innerText = 'Turn for' +Turn;
             }
             
         }
